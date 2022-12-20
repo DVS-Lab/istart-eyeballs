b845143691f91c84b9af5b88a4900b2a630a6dc4
Jimmy Wyngaarden <tun46412@cla18994.tu.temple.edu>
2022-12-19 22:05:09 -0500

    clean up git

--- code/run_L1stats_v2.sh
+++ code/run_L1stats_v2.sh
@@ -6,11 +6,11 @@ basedir="$(dirname "$scriptdir")"
 nruns=1
 
 #for task in socialdoors; do
-for task in ugdg; do
+for task in doors socialdoors; do
 	for ppi in "eyeball_left" "eyeball_right"; do
 		#for sub in 1303 3101 3116 3122 3125 3140 3143 3152 3166 3167 3170 3173 3176 3189 3190 3199 3200 3206 3210 3212 3218 3220 3223; do
-		#for sub in 1001; do
-		for sub in `cat ${basedir}/code/newsubs.txt`; do
+		for sub in 3101; do
+		#for sub in `cat ${basedir}/code/newsubs.txt`; do
 	  		#for run in `seq $nruns`; do
 	  		for run in 1 2; do
 
