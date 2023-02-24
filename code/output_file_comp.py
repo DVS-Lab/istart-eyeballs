6eb40f82789ebfb405f8fdd4019df1feb41388a0
Jimmy Wyngaarden <tun46412@cla18994.tu.temple.edu>
2023-02-24 12:44:42 -0500

    clean up code dir

---
+++ code/output_file_comp.py
@@ -0,0 +1,10 @@
+## create function for returning instances in a given file 
+def idFreq(fileName, target) :
+	
+	file = open(filename, 'r')
+	
+	text = file.read
+	
+	return text.count(target)
+	
+	print(idFreq(extractCBcoopersCopy-2.txt, 'r')
