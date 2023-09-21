b8f1e993744fde14a703714c982d3685869dd4b6
Jimmy Wyngaarden <james.wyngaarden@temple.edu>
2023-09-21 16:29:56 -0400

    troubleshoot eyeMovements

--- code/plot_eyeMovements.m
+++ code/plot_eyeMovements.m
@@ -25,108 +25,127 @@ task = {'doors', 'socialdoors', 'ugdg', 'mid', 'sharedreward'};
 run = {'1', '2'};
 
 % Preallocate dataframes
-df=zeros(length(sub),17);
+%df=zeros(length(sub),17);
 
 % Read files
-for h=1:length(hemi)
-    for s=1:length(sub)
-    df(s,1)=str2double(sub(1,s));
-        for t=1:length(task)
-            for r=1:length(run)
-                f = fullfile(datadir, ['sub-' sub{s}], ['ts_task-' task{t} '_mask-eyeball_' hemi{h} '_run-' run{r} '.txt']);
-                if isfile(f)
-                    data = readmatrix(f);
-                    coV = std(data)/mean(data);
-                    if strcmp(hemi{h},'left')
-                        if strcmp(task{t},'doors')
-                            df(s,2)=coV;
-                        elseif strcmp(task{t},'socialdoors')
-                            df(s,3)=coV;
-                        elseif strcmp(task{t},'ugdg') && strcmp(run{r},'1')
-                            df(s,4)=coV;
-                        elseif strcmp(task{t},'ugdg') && strcmp(run{r},'2')
-                            df(s,5)=coV;
-                        elseif strcmp(task{t},'mid') && strcmp(run{r},'1')
-                            df(s,6)=coV;
-                        elseif strcmp(task{t},'mid') && strcmp(run{r},'2')
-                            df(s,7)=coV;
-                        elseif strcmp(task{t},'sharedreward') && strcmp(run{r},'1')
-                            df(s,8)=coV;
-                        elseif strcmp(task{t},'sharedreward') && strcmp(run{r},'2')
-                            df(s,9)=coV;
-                        end
-                    else
-                        if strcmp(task{t},'doors')
-                            df(s,10)=coV;
-                        elseif strcmp(task{t},'socialdoors')
-                            df(s,11)=coV;
-                        elseif strcmp(task{t},'ugdg') && strcmp(run{r},'1')
-                            df(s,12)=coV;
-                        elseif strcmp(task{t},'ugdg') && strcmp(run{r},'2')
-                            df(s,13)=coV;
-                        elseif strcmp(task{t},'mid') && strcmp(run{r},'1')
-                            df(s,14)=coV;
-                        elseif strcmp(task{t},'mid') && strcmp(run{r},'2')
-                            df(s,15)=coV;
-                        elseif strcmp(task{t},'sharedreward') && strcmp(run{r},'1')
-                            df(s,16)=coV;
-                        elseif strcmp(task{t},'sharedreward') && strcmp(run{r},'2')
-                            df(s,17)=coV;
-                        end
-                    end
-                else
-                    disp("No data for "+f);
-                    if strcmp(hemi{h},'left')
-                        if strcmp(task{t},'doors') && strcmp(run{r},'1')
-                            df(s,2)=NaN;
-                        elseif strcmp(task{t},'socialdoors') && strcmp(run{r},'1')
-                            df(s,3)=NaN;
-                        elseif strcmp(task{t},'ugdg') && strcmp(run{r},'1')
-                            df(s,4)=NaN;
-                        elseif strcmp(task{t},'ugdg') && strcmp(run{r},'2')
-                            df(s,5)=NaN;
-                        elseif strcmp(task{t},'mid') && strcmp(run{r},'1')
-                            df(s,6)=NaN;
-                        elseif strcmp(task{t},'mid') && strcmp(run{r},'2')
-                            df(s,7)=NaN;
-                        elseif strcmp(task{t},'sharedreward') && strcmp(run{r},'1')
-                            df(s,8)=NaN;
-                        elseif strcmp(task{t},'sharedreward') && strcmp(run{r},'2')
-                            df(s,9)=NaN;
-                        end
-                    else
-                        if strcmp(task{t},'doors') && strcmp(run{r},'1')
-                            df(s,10)=NaN;
-                        elseif strcmp(task{t},'socialdoors') && strcmp(run{r},'1')
-                            df(s,11)=NaN;
-                        elseif strcmp(task{t},'ugdg') && strcmp(run{r},'1')
-                            df(s,12)=NaN;
-                        elseif strcmp(task{t},'ugdg') && strcmp(run{r},'2')
-                            df(s,13)=NaN;
-                        elseif strcmp(task{t},'mid') && strcmp(run{r},'1')
-                            df(s,14)=NaN;
-                        elseif strcmp(task{t},'mid') && strcmp(run{r},'2')
-                            df(s,15)=NaN;
-                        elseif strcmp(task{t},'sharedreward') && strcmp(run{r},'1')
-                            df(s,16)=NaN;
-                        elseif strcmp(task{t},'sharedreward') && strcmp(run{r},'2')
-                            df(s,17)=NaN;
-                        end
-                    end
-                end
-            end
-        end
-    end
-end
+% Turn this on to re-generate eyeMovements.xlsx from scratch
+% for h=1:length(hemi)
+%     for s=1:length(sub)
+%     df(s,1)=str2double(sub(1,s));
+%         for t=1:length(task)
+%             for r=1:length(run)
+%                 
+                  % Question: are these txt files pulling the eig
+                  % extractions or the avg extr? 
+%                 f = fullfile(datadir, ['sub-' sub{s}], ['ts_task-' task{t} '_mask-eyeball_' hemi{h} '_run-' run{r} '.txt']);
+%                 if isfile(f)
+%                     data = readmatrix(f);
+%                     coV = std(data)/mean(data);
+                      if coV > 90000 || coV < -90000
+                          disp("debug strange coV value");
+                          keyboard
+                      end  
+%                     if strcmp(hemi{h},'left')
+%                         if strcmp(task{t},'doors')
+%                             df(s,2)=coV;
+%                         elseif strcmp(task{t},'socialdoors')
+%                             df(s,3)=coV;
+%                         elseif strcmp(task{t},'ugdg') && strcmp(run{r},'1')
+%                             df(s,4)=coV;
+%                         elseif strcmp(task{t},'ugdg') && strcmp(run{r},'2')
+%                             df(s,5)=coV;
+%                         elseif strcmp(task{t},'mid') && strcmp(run{r},'1')
+%                             df(s,6)=coV;
+%                         elseif strcmp(task{t},'mid') && strcmp(run{r},'2')
+%                             df(s,7)=coV;
+%                         elseif strcmp(task{t},'sharedreward') && strcmp(run{r},'1')
+%                             df(s,8)=coV;
+%                         elseif strcmp(task{t},'sharedreward') && strcmp(run{r},'2')
+%                             df(s,9)=coV;
+%                         end
+%                     else
+%                         if strcmp(task{t},'doors')
+%                             df(s,10)=coV;
+%                         elseif strcmp(task{t},'socialdoors')
+%                             df(s,11)=coV;
+%                         elseif strcmp(task{t},'ugdg') && strcmp(run{r},'1')
+%                             df(s,12)=coV;
+%                         elseif strcmp(task{t},'ugdg') && strcmp(run{r},'2')
+%                             df(s,13)=coV;
+%                         elseif strcmp(task{t},'mid') && strcmp(run{r},'1')
+%                             df(s,14)=coV;
+%                         elseif strcmp(task{t},'mid') && strcmp(run{r},'2')
+%                             df(s,15)=coV;
+%                         elseif strcmp(task{t},'sharedreward') && strcmp(run{r},'1')
+%                             df(s,16)=coV;
+%                         elseif strcmp(task{t},'sharedreward') && strcmp(run{r},'2')
+%                             df(s,17)=coV;
+%                         end
+%                     end
+%                 else
+%                     disp("No data for "+f);
+%                     if strcmp(hemi{h},'left')
+%                         if strcmp(task{t},'doors') && strcmp(run{r},'1')
+%                             df(s,2)=NaN;
+%                         elseif strcmp(task{t},'socialdoors') && strcmp(run{r},'1')
+%                             df(s,3)=NaN;
+%                         elseif strcmp(task{t},'ugdg') && strcmp(run{r},'1')
+%                             df(s,4)=NaN;
+%                         elseif strcmp(task{t},'ugdg') && strcmp(run{r},'2')
+%                             df(s,5)=NaN;
+%                         elseif strcmp(task{t},'mid') && strcmp(run{r},'1')
+%                             df(s,6)=NaN;
+%                         elseif strcmp(task{t},'mid') && strcmp(run{r},'2')
+%                             df(s,7)=NaN;
+%                         elseif strcmp(task{t},'sharedreward') && strcmp(run{r},'1')
+%                             df(s,8)=NaN;
+%                         elseif strcmp(task{t},'sharedreward') && strcmp(run{r},'2')
+%                             df(s,9)=NaN;
+%                         end
+%                     else
+%                         if strcmp(task{t},'doors') && strcmp(run{r},'1')
+%                             df(s,10)=NaN;
+%                         elseif strcmp(task{t},'socialdoors') && strcmp(run{r},'1')
+%                             df(s,11)=NaN;
+%                         elseif strcmp(task{t},'ugdg') && strcmp(run{r},'1')
+%                             df(s,12)=NaN;
+%                         elseif strcmp(task{t},'ugdg') && strcmp(run{r},'2')
+%                             df(s,13)=NaN;
+%                         elseif strcmp(task{t},'mid') && strcmp(run{r},'1')
+%                             df(s,14)=NaN;
+%                         elseif strcmp(task{t},'mid') && strcmp(run{r},'2')
+%                             df(s,15)=NaN;
+%                         elseif strcmp(task{t},'sharedreward') && strcmp(run{r},'1')
+%                             df(s,16)=NaN;
+%                         elseif strcmp(task{t},'sharedreward') && strcmp(run{r},'2')
+%                             df(s,17)=NaN;
+%                         end
+%                     end
+%                 end
+%             end
+%         end
+%     end
+% end
+% Test; check raw values on some of these subs and see whether they match
+% the values in the output file;
+% 
 
+%df_bar=readmatrix("/ZPOOL/data/projects/istart-eyeballs/code/eyeMovements.xlsx");
+df_bar=readmatrix("/Users/jameswyngaarden/Documents/GitHub/istart-eyeballs/code/eyeMovements.xlsx");
 
-
-df_bar=readmatrix("/ZPOOL/data/projects/istart-eyeballs/eyeMovements.xlsx");
 for c=1:23
-    df_bar(55,c)=mean(df_bar(:,c)/sqrt(nsub));
+    %df_bar(c,:)
+    df_bar(55,c)=median(df_bar(:,c),'omitnan');
+    %df_bar(55,c)=mean(df_bar(:,c)/sqrt(nsub)); %CoV
 end
+
+%df_bar=(mean(df_bar,'omitnan')/std(df_bar,'omitnan'));
+
+
 df_left=df_bar(:,[2:3 6 9 12]);
 df_right=df_bar(:,[13:14 17 20 23]);
+%composite_susd = zscore(mania_raw)+zscore(depress_raw);
+%figure, histogram(composite_susd,50); title('Composite SUSD')
 
 figure
 x = 1:5;
@@ -144,21 +163,21 @@ xticks(1:5);
 xticklabels({'Monetary', 'Social', 'UGDG', 'MID', 'Shared Reward' });
 ylabel('Avg Coefficient of Variation');
 
-low=readmatrix("/ZPOOL/data/projects/istart-eyeballs/derivatives/fsl/sub-1253/ts_task-ugdg_mask-eyeball_left_run-1.txt");
-high=readmatrix("/ZPOOL/data/projects/istart-eyeballs/derivatives/fsl/sub-1302/ts_task-mid_mask-eyeball_right_run-1.txt");
-zlow=(low-mean(low))/std(low);
-zhigh=(high-mean(high))/std(high);
-
-figure
-plot(1:length(zlow),zlow);
-title("Low variability in eye movements")
-xlabel("Time (s)")
-ylabel("BOLD Signal (zscore)")
-ylim([-5 8])
-
-figure
-plot(1:length(zhigh),zhigh);
-title("High variability in eye movements")
-xlabel("Time (s)")
-ylabel("BOLD Signal (zscore)")
-ylim([-5 8])
+% low=readmatrix("/ZPOOL/data/projects/istart-eyeballs/derivatives/fsl/sub-1253/ts_task-ugdg_mask-eyeball_left_run-1.txt");
+% high=readmatrix("/ZPOOL/data/projects/istart-eyeballs/derivatives/fsl/sub-1302/ts_task-mid_mask-eyeball_right_run-1.txt");
+% zlow=(low-mean(low))/std(low);
+% zhigh=(high-mean(high))/std(high);
+% 
+% figure
+% plot(1:length(zlow),zlow);
+% title("Low variability in eye movements")
+% xlabel("Time (s)")
+% ylabel("BOLD Signal (zscore)")
+% ylim([-5 8])
+% 
+% figure
+% plot(1:length(zhigh),zhigh);
+% title("High variability in eye movements")
+% xlabel("Time (s)")
+% ylabel("BOLD Signal (zscore)")
+% ylim([-5 8])
