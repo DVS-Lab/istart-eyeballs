3c0768123ca4b9deead674ed83cf3f114a610038
jameswyngaarden <james.wyngaarden@temple.edu>
2023-09-25 18:45:16 -0400

    add ZPOOL to path

--- code/L1stats.sh
+++ code/L1stats.sh
@@ -12,7 +12,7 @@
 # ensure paths are correct irrespective from where user runs the script
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 maindir="$(dirname "$scriptdir")"
-istartdatadir=/data/projects/istart-data
+istartdatadir=/ZPOOL/data/projects/istart-data
 
 # study-specific inputs
 sm=5
