1ffc360ca195ce9c866379446122fcb42015fad8
jameswyngaarden <james.wyngaarden@temple.edu>
2023-09-25 18:41:35 -0400

    re-run eig

--- code/run_L1stats.sh
+++ code/run_L1stats.sh
@@ -9,11 +9,11 @@ nruns=1
 for task in doors socialdoors mid sharedreward ugdg; do
 	for ppi in "eyeball_left" "eyeball_right"; do
 		#for sub in 1303 3101 3116 3122 3125 3140 3143 3152 3166 3167 3170 3173 3176 3189 3190 3199 3200 3206 3210 3212 3218 3220 3223; do
-		for sub in 1002 1007; do
-		#for sub in `cat ${basedir}/code/newsubs.txt`; do
+		#for sub in 1002 1007; do
+		for sub in `cat ${basedir}/code/newsubs.txt`; do
 	  		#for run in `seq $nruns`; do
 	  		for run in 1 2; do
-	  			for eig in 0; do
+	  			for eig in 1; do
 
 		  				# Manages the number of jobs and cores
 		  				SCRIPTNAME=${basedir}/code/L1stats.sh
